40ec61f7635c747a3cb5452310c1148a797c9932
David Wang <devtesting404@gmail.com>
2024-01-03 14:13:54 +0800

    feat: set up basic main.cpp

---
+++ srcs/main.cpp
@@ -0,0 +1,24 @@
+#include "scop.hpp"
+#include "App.hpp"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2){
+		std::cerr << "./scop <filename>.obj" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	try
+	{
+		Scop::App app;
+
+		app.run(argv[1]);
+	}
+	catch(const std::runtime_error& e)
+	{
+		std::cerr << e.what() << '\n';
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
