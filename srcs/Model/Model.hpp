df8113ddb0f533b50cd7784240f01d2b7a4bf1b6
Wang Khang Waei <devtesting404@gmail.com>
2024-01-14 01:06:13 +0800

    update: add constructors

--- srcs/Model/Model.hpp
+++ srcs/Model/Model.hpp
@@ -13,12 +13,16 @@ namespace Scop
         float y;
         float z;
         float w = 1.0f;
+
+        Vertex(float x, float y, float z, float w = 1.0f): x(x), y(y), z(z), w(w) {}
     };
 
     struct FaceData {
         int vertex_index;
         int texture_vertex_index = 0;
         int vertex_normal_index = 0;
+
+        FaceData(int vertex_index, int texture_vertex_index = 0, int vertex_normal_index = 0): vertex_index(vertex_index), texture_vertex_index(texture_vertex_index), vertex_normal_index(vertex_normal_index) {}
     };
 
     struct Face {
