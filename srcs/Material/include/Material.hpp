91d5af6a082403862fc09d7b15f68049bfc305a7
David Wang <devtesting404@gmail.com>
2024-01-17 08:40:14 +0800

    chore: add missing member function declartions

--- srcs/Material/include/Material.hpp
+++ srcs/Material/include/Material.hpp
@@ -18,22 +18,27 @@ namespace Scop
 
     // https://paulbourke.net/dataformats/mtl/
     struct Material
-    {
-        std::string material_name; // if no newmtl name define in file it is invalid; NO BLANKS
-        RGB ambient_reflectivity; // 0 - 1.0 Ka // color // other formats include Ka spectral file.rfl factor // Ka xyz x y z
-        RGB diffuse_reflectivity; // 0 - 1.0 Kd // transparency
-        RGB specular_reflectivity; // 0 - 1.0 Ks // reflectivity
-        float specular_exponent; // 0 - 1000 // Ns
-        int illumination_model; // 1 - 10 need illum_ prepended?? // illum
-        float dissolve; // 0 - 1.0 // d
-        float optical_density; // 0.001 to 10 // Ni
+    {   
+        public:
+            std::string material_name; // if no newmtl name define in file it is invalid; NO BLANKS
+            RGB ambient_reflectivity; // 0 - 1.0 Ka // color // other formats include Ka spectral file.rfl factor // Ka xyz x y z
+            RGB diffuse_reflectivity; // 0 - 1.0 Kd // transparency
+            RGB specular_reflectivity; // 0 - 1.0 Ks // reflectivity
+            float specular_exponent; // 0 - 1000 // Ns
+            int illumination_model; // 1 - 10 need illum_ prepended?? // illum
+            float dissolve; // 0 - 1.0 // d
+            float optical_density; // 0.001 to 10 // Ni
 
-        Material() = delete;
-        Material(const std::string &filename);
-        ~Material();
+            Material() = delete;
+            Material(const std::string &filename);
+            ~Material();
 
-        void check_valid_extension(const std::string& filename);
-        void parse_mtl_file_to_instance(const std::string& filename);
+        private:
+            void parse_material_file(const std::string &filename);
+            void check_valid_extension(const std::string& filename);
+            void parse_mtl_file_to_instance(const std::string& filename);
+            void parse_obj_file_line(const std::string &line);
+            void parse_reflectivity(const std::vector<std::string> &line_tokens, RGB &reflectivity);
     };
 }
 
